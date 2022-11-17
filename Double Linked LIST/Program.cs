890a6f52c33e69a031165441424086390d158694
panjialbir <panjialbir10@gmail.com>
2022-11-17 17:15:14 +0700

    membuat class doublelinkedlist

--- Double Linked LIST/Program.cs
+++ Double Linked LIST/Program.cs
@@ -18,3 +18,36 @@ namespace Double_Linked_List_Algorithm
         public Node next;
         public Node prev;
     }
+    class DoubleLinkedList
+    {
+        Node START;
+        public DoubleLinkedList()
+        {
+            START = null;
+        }
+        public void addNode()
+        {
+            int rollNo;
+            string nm;
+            Console.Write("\nEnter the roll number of the student: ");
+            rollNo = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\nEnter the name of the student: ");
+            nm = (Console.ReadLine());
+            Node newnode = new Node();
+            newnode.rollNumber = rollNo;
+            newnode.name = nm;
+
+            if (START == null || rollNo <= START.rollNumber)
+            {
+                if ((START != null) && (rollNo == START.rollNumber))
+                {
+                    Console.WriteLine("\nDuplicate roll numbers not allowed");
+                    return;
+                }
+                newnode.next = START;
+                if (START != null)
+                    START.prev = newnode;
+                newnode.prev = null;
+                START = newnode;
+                return;
+            }
